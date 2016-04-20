8cf92c4219258201d9bf3ae64c3328ef1f249460
Archit Kansal <archit.knsl@gmail.com>
2016-04-21 00:04:34 +0530

    modified server files added

--- Udhaar-db/get_list.php
+++ Udhaar-db/get_list.php
@@ -8,7 +8,9 @@
      		$response = array();
         $response['person'] = array();
 
-    		$q2 = "select mob_no from user where (id in (select id1 from trans where id2='$id')) or (id in (select id2 from trans where id1='$id'))";
+    		// $q2 = "select id,mob_no from user where (id in (select id1 from trans where id2='$id')) or (id in (select id2 from trans where id1='$id'))";
+
+        $q2 = "select id,mob_no,money,tym from user,trans where (user.id = trans.id1 and trans.id2 = '$id') or (user.id = trans.id2 and trans.id1 = '$id') ;";
 
      		$mo = mysqli_query($conn, $q2);
         $i=0;
@@ -18,7 +20,10 @@
      			{
             $person = array();
             $i++;
-     			  $person['mob_no'] = $row['mob_no'];
+     		    $person['id'] = $row['id'];
+            $person['mob_no'] = $row['mob_no'];
+            $person['money'] = $row['money'];
+            $person['tym'] = $row['tym'];
             array_push($response['person'],$person);
      			}
 
@@ -44,3 +49,4 @@
 
 ?>
 
+                                                                                                                                                                                                 
--- Udhaar-db/popupmoneygive.php
+++ Udhaar-db/popupmoneygive.php
@@ -3,6 +3,7 @@
     $response = array();
      if( isset($_POST['txtid1']) && isset($_POST['txtmob']) && isset($_POST['txtmoney']) )
     {
+        $type = 0;
         $money = $_POST['txtmoney'];
         $id1=$_POST['txtid1'];
         $id2=$_POST['txtmob'];
@@ -15,7 +16,7 @@
         $res=mysqli_fetch_array($result);
 
         $id2=$res['id']; 
-
+        // echo $id2;
         $query = "Select id1,id2,money from
         trans where (id1='$id1' and id2='$id2') or (id1='$id2' and id2='$id1') "; 
         
@@ -24,25 +25,38 @@
         $res=mysqli_fetch_array($result);
 
         $m = $res['money'];
+
+        // echo $res['money'];
+
         $finalmoney=0;
         if($id==$res['id1'])               //  1-->2 ---->>>> Positive  ---->>> give
         {
+            $type=1;
             $finalmoney = $m + $money;
         }
         else                        //  2-->1 ---->>>> Negative  ---->>> give
         {
+            $type=-1;
             $finalmoney = $m - $money;
         }
+        // echo $finalmoney;
 
-         $query = "update trans set money = $finalmoney where (id1='$id1' and id2='$id2')
-                   or (id1='$id2' and id2='$id1')"; 
+         $query = "update trans set money = $finalmoney 
+                      where (id1='$id1' and id2='$id2') or (id1='$id2' and id2='$id1'); ";
         
         $result = mysqli_query($conn, $query);
 
         if($result)
         {
+            $q = "update trans set tym=CURRENT_TIMESTAMP where (id1='$id1' and id2='$id2') or (id1='$id2' and id2='$id1');";
+            $result = mysqli_query($conn, $query);
+            date_default_timezone_set('Asia/Kolkata');
             $response['success']=1;
-            $response['finalmoney']=$finalmoney;
+            if($type==1)
+                $response['money']=$finalmoney;
+            else
+                $response['money']=(-1)*$finalmoney;
+            $response['tym'] = date('Y-m-d G-i-s');
             echo json_encode($response);
         }
         else
@@ -63,4 +77,17 @@
 
 
 ?>
+
+
+<html>
+<head><title>Login</title></head>
+    <body>
+        <form action="<?PHP $_PHP_SELF ?>" method="post">
+            ID <input type="text" name="txtid1" value="" /><br/>
+            mob <input type="text" name="txtmob" value="" /><br/>
+            money <input type="text" name="txtmoney" value="" /><br/>
+            <input type="submit" name="btnSubmit" value="Login"/>
+        </form>
+    </body>
+</html>
 

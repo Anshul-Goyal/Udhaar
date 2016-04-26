20e4a2cd1455da11308705e51385979de54a369c
Anshul-Goyal <anshul.goyal7@gmail.com>
2016-04-26 13:07:23 +0530

    Orientation corrected , Back button working

--- udhaar/app/src/main/java/com/udhaar/udhaar/userhome.java
+++ udhaar/app/src/main/java/com/udhaar/udhaar/userhome.java
@@ -42,6 +42,8 @@ public class userhome extends ListActivity implements AsyncResponse {
 
     Map<String, String> cmap = new HashMap<String, String>();
     String namearray[];
+    public static int var=0;
+    public static userhome uhobj;
     private SharedPreferences preferenceSettings;
     private SharedPreferences.Editor preferenceEditor;
     String txtid="";
@@ -65,6 +67,11 @@ public class userhome extends ListActivity implements AsyncResponse {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userhome);
+        uhobj=this;
+        if(var!=123)
+        var=0;
+        else
+        var=1;
 
         preferenceSettings = getPreferences(mode);
         String id = PreferenceManager.getDefaultSharedPreferences(userhome.this).getString("txtid", "NULL");
@@ -141,7 +148,9 @@ public class userhome extends ListActivity implements AsyncResponse {
                 i.putExtra("cnum", cmap.get(name));
                 System.out.println("Number is ::::::::: " + cmap.get(name));
                 startActivity(i);
+                if(var != 0)
                 userhome.this.finish();
+                var++;
 
             }
         });

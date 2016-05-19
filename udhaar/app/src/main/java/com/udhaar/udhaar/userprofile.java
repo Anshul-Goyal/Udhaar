b32760bdaeefacb04e19c57d52e54bf95ede85bb
Anshul-Goyal <anshul.goyal7@gmail.com>
2016-05-19 12:20:07 +0530

    Login page changed and calc added

--- udhaar/app/src/main/java/com/udhaar/udhaar/userprofile.java
+++ udhaar/app/src/main/java/com/udhaar/udhaar/userprofile.java
@@ -9,6 +9,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
+import java.util.HashMap;
+import java.util.ArrayList;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageInfo;
+import java.util.List;
+import android.content.pm.ResolveInfo;
+import java.util.Collections;
+import android.content.pm.ActivityInfo;
+import android.content.ComponentName;
 
 public class userprofile extends AppCompatActivity {
     String cnum;
@@ -94,5 +103,57 @@ public class userprofile extends AppCompatActivity {
         startActivity(i);
     }
 
+    public void opencalc (View view) {
+        System.out.println("helloinsideopencalc");
+        ArrayList<HashMap<String,Object>> items =new ArrayList<HashMap<String,Object>>();
+       //PackageManager pm;
+        final PackageManager pm = getPackageManager();
+        List<PackageInfo> packs = pm.getInstalledPackages(0);
+        for (PackageInfo pi : packs) {
+            if( pi.packageName.toString().toLowerCase().contains("calcul")){
+                HashMap<String, Object> map = new HashMap<String, Object>();
+                map.put("appName", pi.applicationInfo.loadLabel(pm));
+                map.put("packageName", pi.packageName);
+                items.add(map);
+            }
+        }
+
+        if(items.size()>=1){
+
+            String packageName = (String) items.get(0).get("packageName");
+            Intent i = pm.getLaunchIntentForPackage(packageName);
+            if (i != null)
+                startActivity(i);
+            else{
+                Intent intent = new Intent();
+                intent.setPackage(packageName);
+
+                PackageManager p = getPackageManager();
+                List<ResolveInfo> resolveInfos = p.queryIntentActivities(intent, 0);
+                Collections.sort(resolveInfos, new ResolveInfo.DisplayNameComparator(p));
+
+                if(resolveInfos.size() > 0) {
+                    ResolveInfo launchable = resolveInfos.get(0);
+                    ActivityInfo activity = launchable.activityInfo;
+                    ComponentName name=new ComponentName(activity.applicationInfo.packageName,
+                            activity.name);
+                    Intent it=new Intent(Intent.ACTION_MAIN);
+
+                    it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                            Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+                    it.setComponent(name);
+
+                    startActivity(it);
+                }
+            }
+
+              //  System.out.println("found "+packageName);
+
+        }
+        else{
+            // Application not found
+            System.out.println("Calculator cannot be opened");
+        }
+    }
 
 }
